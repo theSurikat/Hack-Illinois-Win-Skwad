b946adca9186479475e0236630132ab06fd0b6a0
Grant Everett <Geverett94@gmail.com>
2016-02-20 21:00:41 -0600

    get, post and delete

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var pg = require('pg');
 var connectionString = require(path.join(__dirname, '../', '../', 'config'));
 
 
-router.post('/', function(req1, req2 res) {
+router.post('/', function(req1, req2, res) {
 
     var results = [];
 
@@ -24,7 +24,7 @@ router.post('/', function(req1, req2 res) {
         client.query("INSERT INTO partnerships(userId1, userId2) values($1, $2)", [data.use1, data.use2]);
 
         // SQL Query > Select Data
-        var query = client.query("SELECT * FROM items ORDER BY id ASC");
+        var query = client.query("SELECT * FROM partnerships");
 
         // Stream results back one row at a time
         query.on('row', function(row) {
@@ -39,4 +39,72 @@ router.post('/', function(req1, req2 res) {
 
 
     });
+});
+
+router.get('/', function(req, res) {
+
+    var results = [];
+
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, function(err, client, done) {
+        // Handle connection errors
+        if(err) {
+          done();
+          console.log(err);
+          return res.status(500).json({ success: false, data: err});
+        }
+
+        // SQL Query > Select Data
+        var query = client.query("SELECT * FROM partnerships;");
+
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            done();
+            return res.json(results);
+        });
+
+    });
+
+});
+
+router.delete('/api/v1/todos/:todo_id', function(req, res) {
+
+    var results = [];
+
+    // Grab data from the URL parameters
+    var id = req.params.todo_id;
+
+
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, function(err, client, done) {
+        // Handle connection errors
+        if(err) {
+          done();
+          console.log(err);
+          return res.status(500).json({ success: false, data: err});
+        }
+
+        // SQL Query > Delete Data
+        client.query("DELETE FROM partnerships WHERE userId1=$1 AND userId2=$2", [use1 use2]);
+
+        // SQL Query > Select Data
+        var query = client.query("SELECT * FROM partnerships");
+
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            done();
+            return res.json(results);
+        });
+    });
+
 });
